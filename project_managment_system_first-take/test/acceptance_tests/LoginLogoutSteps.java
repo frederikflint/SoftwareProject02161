40f59b7bff0a66384b579bcf62abed54bcd0363b
Fehrnovic <s174281@student.dtu.dk>
2018-05-01 16:04:31 +0200

    Login test

--- project_managment_system_first-take/test/acceptance_tests/LoginLogoutSteps.java
+++ project_managment_system_first-take/test/acceptance_tests/LoginLogoutSteps.java
@@ -1,40 +1,72 @@
 package acceptance_tests;
 
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import planner.app.Planner;
+import planner.domain.Developer;
 
 public class LoginLogoutSteps {
 
     private Planner planner;
+    private Developer developer = new Developer("dd","1234");
 
     public LoginLogoutSteps(Planner planner) {
         this.planner = planner;
     }
 
-    @Given("^that there is no active user$")
-    public void thatThereIsNoActiveUser() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-    }
+    @Given("^that there is no active user on the system$")
+    public void thatThereIsNoActiveUserOnTheSystem() throws Exception {
+        assertThat(planner.activeUser,is(equalTo(null)));
+        }
 
     @Given("^the credentials is \"([^\"]*)\" and the password is \"([^\"]*)\"$")
-    public void theCredentialsIsAndThePasswordIs(String arg1, String arg2) throws Exception {
-
+    public void theCredentialsIsAndThePasswordIs(String credentials, String password) throws Exception {
+        // TODO: Make this a helper
+        planner.developers.add(developer);
+        assertThat(developer.getCredentials(),is(equalTo(credentials)));
+        assertThat(developer.getPassword(),is(equalTo(password)));
     }
 
-    @When("^the admin login succeeds$")
-    public void theAdminLoginSucceeds() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
+    @When("^the developer login succeeds$")
+    public void theDeveloperLoginSucceeds() throws Exception {
+        planner.userLogIn(developer.getCredentials(),developer.getPassword());
+        assertThat(planner.activeUser,is(equalTo(developer)));
     }
 
-    @Then("^the admin is logged in$")
-    public void theAdminIsLoggedIn() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
+    @Then("^the developer is logged in$")
+    public void theDeveloperIsLoggedIn() throws Exception {
+        assertThat(planner.getActiveUser() != null, is(equalTo(true)));
     }
 
+    @Given("^the username is \"([^\"]*)\"$")
+    public void theUsernameIs(String credentials) throws Exception {
+        assertFalse(developer.getCredentials() == credentials);
+    }
+
+    @When("^the developer login fails$")
+    public void theDeveloperLoginFails() throws Exception {
+
+        try{
+            planner.userLogIn(developer.getCredentials(), "MRX");
+        } catch (Exception e){
+            assertThat(e.getMessage(),is(equalTo("No developers registered with the planner")));
+        }
+    }
+
+    @Then("^the developer is not logged in$")
+    public void theDeveloperIsNotLoggedIn() throws Exception {
+        assertTrue(planner.activeUser == null);
+
+    }
 
 }
--- project_managment_system_first-take/use_cases/Developer_Login.feature
+++ project_managment_system_first-take/use_cases/Developer_Login.feature
@@ -5,17 +5,15 @@ Feature: Developer login
   Scenario: Developer can login
     Given that there is no active user on the system
     And the credentials is "dd" and the password is "1234"
-    When the admin login succeeds
-    Then the admin is logged in
+    When the developer login succeeds
+    Then the developer is logged in
 
-#  Scenario: Developer has the wrong username
-#    Given that there is no active user
-#    And the username is "wrong username"
-#    Then the admin login fails
-#    And the admin is not logged in
-#
-#  Scenario: Administrator has the wrong password
-#    Given that there is no active user
-#    And the password is "wrong password"
-#    Then the admin login fails
-#    And the admin is not logged in
+  Scenario: Developer has the wrong username
+    Given the username is "wrong username"
+    When the developer login fails
+    Then I get the error message "Wrong username"
+
+#  Scenario: Developer has the wrong password
+#    Given the password is "wrong password"
+#    When the developer login fails
+#    Then I get the error message "Wrong password"
